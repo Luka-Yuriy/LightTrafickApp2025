634639191cbd8c358a4775861d8d229b38021d6d
Yuri Luka <lukayurii.work@gmail.com>
2024-12-01 21:31:19 +0500

    Work with logic

--- LightTrafickApp2025/ViewController.swift
+++ LightTrafickApp2025/ViewController.swift
@@ -14,15 +14,55 @@ class ViewController: UIViewController {
     @IBOutlet var greenView: UIView!
     @IBOutlet var lightChangeButton: UIButton!
     
+    private var lightTurned: colorOfLight = .red
+    
+    enum colorOfLight {
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        lightChangeButton.layer.cornerRadius = 12
+        
+        lightTurned = .red
         
     }
 
     @IBAction func lightChangeButtonDidTaped() {
         
+        switch lightTurned {
+        case .red:
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+            lightTurned = .yellow
+        case .yellow:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+            lightTurned = .green
+        case .green:
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            lightTurned = .red
+        }
     }
+    
+    
+    
+    
     
 }
 
